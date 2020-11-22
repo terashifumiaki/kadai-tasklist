1368a713b16e9be48cc1b28a5d442f8ed686d261
Terashi.Fumiaki <karakaraponpon@gmail.com>
2020-11-22 23:31:36 +0000

    first

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # トップページ"/"と"/tasks"にアクセスしたときのルーティング
+  root to: 'tasks#index'
+  # 省略形で書きますよ基本の７ルーティング↓
+  resources :tasks
+# ７つを、RESTfulなルーティングというらしい。
 end
